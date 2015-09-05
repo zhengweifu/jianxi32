a9d3037e44c8db05699ef1cbfb088f7ba83a4564
zhengweifu <zhengweifu@bitmap3d.com.cn>
2015-09-06 00:49:10 +0800

    update

--- Application/Home/Controller/JXPhotoController.class.php
+++ Application/Home/Controller/JXPhotoController.class.php
@@ -13,10 +13,8 @@ use Think\Controller;
 class JXPhotoController extends Controller {
     public function index() {
 
-        $pictrues = listDir(APP_PATH . "../Public/jxphoto/imgs/pictrues");
-        for($i=0; $i<count($pictrues); $i++) {
-            $pictrues[$i] = "Public/jxphoto/imgs/pictrues/" . $pictrues[$i];
-        }
+        $pictrues = array("Public/jxphoto/imgs/pictrues/a.jpg", "Public/jxphoto/imgs/pictrues/b.jpg");
+
 
         $this->assign('jxphoto_pictrues', $pictrues);
 
