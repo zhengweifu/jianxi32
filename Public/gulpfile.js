da90a11a33bda16f89257c920699d11d7905b6bf
zhengweifu <zhengweifu@bitmap3d.com.cn>
2016-01-04 13:12:52 +0800

    change gulpfile.js

--- Public/gulpfile.js
+++ Public/gulpfile.js
@@ -29,39 +29,39 @@ gulp.task('styles-sass', function() {
 	//输出压缩文件到指定目录
 	.pipe(gulp.dest("dist/basic/css"))
 	//提醒任务完成
-	.pipe(notify({message: "Styles task complete"}));
+	.pipe(notify({message: "Styles sass task complete"}));
 });
 
 gulp.task('styles-css', function() {
-	gulp.src('src/basic/css/*')
+	gulp.src('src/**/css/*')
 	.pipe(rename({suffix: '.min'}))
 	.pipe(minifycss())
-	.pipe(gulp.dest("dist/basic/css"))
+	.pipe(gulp.dest("dist/"))
 });
 
 gulp.task("scripts", function() {
-	gulp.src('src/basic/js/*.js')
+	gulp.src('src/**/js/*.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'))
 	.pipe(rename({ suffix: '.min' }))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/basic/js'))
-	.pipe(notify({message: 'Scripts task complete'}));
+	.pipe(gulp.dest('dist/'))
+	.pipe(notify({message: 'Scripts cass task complete'}));
 });
 
 // Images
 gulp.task('images', function() {
-	gulp.src('src/basic/imgs/*')
+	gulp.src('src/**/imgs/*')
 	.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-	.pipe(gulp.dest('dist/basic/imgs/'))
+	.pipe(gulp.dest('dist/'))
 	.pipe(notify({message: 'Images task complete'}));
 });
 
 // Copys
 gulp.task('copys', function() {
-	gulp.src('src/basic/fonts/*')
+	gulp.src('src/**/fonts/*')
 	.pipe(rename({ suffix: '' }))
-	.pipe(gulp.dest('dist/basic/fonts'))
+	.pipe(gulp.dest('dist/'))
 	.pipe(notify({message: 'Copys task complete'}));
 });
 
@@ -71,5 +71,9 @@ gulp.task('default', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/**/*.scss', ['styles'])
+	gulp.watch('src/**/sass/*.scss', ['styles-sass']);
+	gulp.watch('src/**/js/*.js', ['scripts']);
+	gulp.watch('src/**/css/*.css', ['styles-css']);
+	gulp.watch('src/**/imgs/*', ['images']);
+	gulp.watch('src/**/fonts/*', ['copys']);
 });
