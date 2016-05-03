9d53bcd7a61f7c1d339752cfea69a8987610eb87
zhengweifu <zhengweifu@gdi.com.cn>
2016-05-03 18:06:08 +0800

    add material-ui[react]

---
+++ Application/Home/Controller/JYController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: fun.zheng
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+
+class JYController extends Controller {
+    public function index() {
+        $this->display();
+    }
+}
